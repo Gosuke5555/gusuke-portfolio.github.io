4f7198096fb0285228d81ce584d8ffb45015b8f6
Gosuke5555 <72441292+Gosuke5555@users.noreply.github.com>
2021-06-13 16:38:34 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// ハンバーガーメニュー
+$('.burger-btn').on('click',function(){
+  $('.burger-btn').toggleClass('close');
+  $('.nav-wrapper').toggleClass('slide-in');
+  $('body').toggleClass('noscroll');
+});
+
+// スクロールするとフェードイン
+$(function(){
+  $(window).on('load scroll', function() {
+     var winScroll = $(window).scrollTop();
+     var winHeight = $(window).height();
+     var scrollPos = winScroll + (winHeight * 0.8);
+
+     $(".show").each(function() {
+        if($(this).offset().top < scrollPos) {
+           $(this).css({opacity: 1, transform: 'translate(0, 0)'});
+        }
+     });
+  });
+});
