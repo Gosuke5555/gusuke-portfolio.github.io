1040f363211646ef91dcaed64b6ae586988f45b6
Ryosuke Goto <72441292+Gosuke5555@users.noreply.github.com>
2021-08-25 17:14:43 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,28 @@ $('.nav-item').on('click',function(){
    $('body').removeClass('noscroll');
 });
 
+// ファストビューの画像スライド
+$('.slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+  speed:1000,//スライドの動きのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 1,//スライドを画面に3枚見せる
+  slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+  arrows: true,//左右の矢印あり
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: true,//下部ドットナビゲーションの表示
+      pauseOnFocus: false,//フォーカスで一時停止を無効
+      pauseOnHover: false,//マウスホバーで一時停止を無効
+      pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+  });
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+  $('.slider').slick('slickPlay');
+});
+
 // スクロールするとフェードイン
 $(function(){
   $(window).on('load scroll', function() {
@@ -26,7 +48,8 @@ $(function(){
   });
 });
 
-// Contctへのスムーススクロール
+
+// Contactへのスムーススクロール
 $('a[href^="#contact"]').click(function() {
    // スクロールの速度
    let speed = 400; // ミリ秒で記述
